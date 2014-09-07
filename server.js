fb6422036be0088a5a34e76d18ca9b5c3b816d6d
bcoe <bencoe@gmail.com>
2014-09-07 00:54:16 -0700

    starting to build splash page.

---
+++ server.js
@@ -0,0 +1,33 @@
+// lightweight web-server using Restify. Implements
+// a typeahead search interface for packages on npm.
+//
+// start server: npm start
+// build assets: npm run-script build.
+var restify = require('restify'),
+  serverOpts = {},
+  fs = require('fs');
+
+// Support running typeahead service with SSL.
+if (process.env.KEY) {
+  serverOpts.key = fs.readFileSync(process.env.KEY);
+  serverOpts.certificate = fs.readFileSync(process.env.CERTIFICATE);
+}
+
+var server = restify.createServer(serverOpts);
+server.use(restify.queryParser());
+
+// serve static JavaScript and CSS.
+server.get(/\/js|css|images\/?.*/, restify.serveStatic({
+  directory: './assets'
+}));
+
+// serve the static index page.
+server.get(/\/?/, restify.serveStatic({
+  directory: './assets',
+  default: 'index.html'
+}));
+
+// bind server to on port 5000, or the port provided.
+server.listen(process.env.PORT || 5000, function () {
+  console.log('%s listening at %s', server.name, server.url);
+});
