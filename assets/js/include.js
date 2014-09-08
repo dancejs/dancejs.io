8ce80f8c931f1aab865491a4a4d81a572213755c
bcoe <bencoe@gmail.com>
2014-09-07 23:03:27 -0700

    adding profiles.

--- assets/js/include.js
+++ assets/js/include.js
@@ -6,13 +6,13 @@ var skrollr = require('skrollr'),
   $ = jquery = require('jquery');
 
 module.exports = function(opts) {
+  // nifty parallax scrolling lib.
   $(document).ready(function() {
     $('#bottom-info').show();
     skrollr.init();
-
-
   });
 
+  // hover effect on profile pics.
   $('.item').hover(
     function() {
       $(this).children('.profile').show();
